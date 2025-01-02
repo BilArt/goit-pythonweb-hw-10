10401a52415fd38622dd8215bb1b880c56985ff0
Artem Bilousov <artem.bilousov.1993@gmail.com>
2025-01-02 18:08:20 +0100

    updated models.py

--- contacts_api/models.py
+++ contacts_api/models.py
@@ -1,13 +1,22 @@
-from sqlalchemy import Column, Integer, String, Date
-from database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class Contact(Base):
     __tablename__ = "contacts"
-
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    phone = Column(String, nullable=False)
-    birthday = Column(Date, nullable=True)
-    additional_info = Column(String, nullable=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    phone = Column(String)
+    birthday = Column(String)
+    additional_info = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id")) 
+    user = relationship("User", back_populates="contacts")
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    contacts = relationship("Contact", back_populates="user")
