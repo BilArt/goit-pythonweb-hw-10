34ec064252ee261da179d8a259280a5eda52c625
Artem Bilousov <artem.bilousov.1993@gmail.com>
2025-01-02 18:14:29 +0100

    updated auth.py

--- contacts_api/auth.py
+++ contacts_api/auth.py
@@ -7,6 +7,7 @@ from database import get_db
 from models import User
 from schemas import UserCreate, UserResponse, Token
 from utils import hash_password, verify_password
+from email_utils import send_email
 from datetime import datetime, timedelta
 
 SECRET_KEY = config("SECRET_KEY", default="supersecretkey")
@@ -33,17 +34,26 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
 
 
 @auth_router.post("/register", response_model=UserResponse, status_code=201)
-def register_user(user: UserCreate, db: Session = Depends(get_db)):
+async def register_user(user: UserCreate, db: Session = Depends(get_db)):
     # Проверка, существует ли пользователь с таким email
     existing_user = db.query(User).filter(User.email == user.email).first()
     if existing_user:
         raise HTTPException(status_code=409, detail="Email already registered")
     
     hashed_password = hash_password(user.password)
-    new_user = User(email=user.email, password=hashed_password, full_name=user.full_name)
+    new_user = User(email=user.email, password=hashed_password, full_name=user.full_name, is_verified=False)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+
+    verification_link = f"http://127.0.0.1:8000/auth/verify-email?email={new_user.email}"
+    email_body = f"""
+    <h1>Подтверждение email</h1>
+    <p>Для подтверждения вашего аккаунта перейдите по ссылке:</p>
+    <a href="{verification_link}">Подтвердить Email</a>
+    """
+    await send_email("Подтверждение Email", new_user.email, email_body)
+
     return new_user
 
 
@@ -63,3 +73,16 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+@auth_router.get("/verify-email")
+def verify_email(email: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if user.is_verified:
+        return {"message": "Email already verified"}
+    
+    user.is_verified = True
+    db.commit()
+    return {"message": "Email successfully verified"}
